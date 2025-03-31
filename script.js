185d3d4bfa90e5957b95d03ecaadff2b45da7c1c
ViniciusLucca <vinilucca7304@gmail.com>
2025-03-31 13:17:34 -0300

    feat: connect sum function to U.I.
    This changes refactors (sort of) the code, changing some variable names, and creating some functions.
    
    To achieve this, when an operator is typed (via buttons), it splits the string from the operator, saves both the numbers and the operator, and runs operate() with these values.

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-let btnNums = document.querySelectorAll('.btnItem');
+let btnItems = document.querySelectorAll('.btnItem');
+let btnNums = document.querySelectorAll('.btnNum');
+let btnOps = document.querySelectorAll('.btnOperator');
 let display = document.querySelector('#displayText');
+let currentOp;
+let nums = {first: undefined, second: undefined};
 
 btnNums.forEach(element => {
     element.addEventListener('click', type);
 });
+btnOps.forEach(element => {
+    element.addEventListener('click', addOperator);
+})
 document.querySelector('#clear').addEventListener('click', clear);
+document.querySelector('#equals').addEventListener('click', () => operate(nums.first, currentOp, nums.second));
 
+function addOperator(e){
+    if (currentOp == undefined) {
+        type(e);
+    }
+    else {
+        if (isNaN(+display.value[display.value.length - 1]))
+        {
+            display.value = toString(Array.from(display.value).splice(display.value.length - 1, 1));
+        } else{
+            nums.first = +((display.value).split(e.target.value))[0];
+            nums.second = +((display.value).split(e.target.value))[1];
+            console.log(operate(nums.first, currentOp, nums.second))
+        }
+    }
+    currentOp = e.target.value;
+
+}
 function type(e){
-    display.value +=  e.target.value;
+    let char = e.target.value;
+    display.value += char;
 }
 
 function clear(){
@@ -33,7 +59,7 @@ function divide(num1, num2){
 function operate(num1, operator, num2){
     switch (operator) {
         case "+":
-            add(num1, num2);
+            console.log(add(num1, num2));
             break;
         case "-":
             subtract(num1, num2);
